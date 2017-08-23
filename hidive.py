93f2841e9ba91f3f74a66fcb4fac0954faf69e92
Kayce Danna <kayce.danna@returnpath.com>
2017-08-23 16:43:27 -0600

    going to make a sep class for each venue

---
+++ hidive.py
@@ -0,0 +1,42 @@
+# import libraries
+import urllib2
+import logging
+from bs4 import BeautifulSoup
+
+class HiDiveShows(object):
+
+    def __init__(self, venue_name, venue_url):
+        self.venue_name = venue_name
+        self.venue_url = venue_url
+
+    # make beautiful soup
+    def getBSObject(self):
+        # query the website and return the results to a new variable
+        page = urllib2.urlopen(self.venue_url).read()
+        # use BeautulSoup to parse the html and store its contents into a new variable
+        soup = BeautifulSoup(page, 'html.parser')
+        # this will create a BS concert section object that is iterable
+        concerts = soup.find_all('div', attrs={'class': "list-view-item"})
+        return concerts
+        # print concerts.prettify()
+
+    # create list of lists contain each concert event from BS
+    def getConcertListings(self):
+        all_concert_listings = [] # create a master list of all the concert event listings from sing_concert_listing
+        for concert in self.getBSObject():
+            try:
+                single_concert_listing = []  # create a concert list for each event
+                headliner = concert.find('h1', attrs={'class': 'headliners summary'}).text.strip()
+                single_concert_listing.append(headliner)
+                opening_act = concert.find('h2', attrs={'class': 'supports description'}).text.strip()
+                single_concert_listing.append(opening_act)
+                date = concert.find('h2', attrs={'class': 'dates'}).text.strip()
+                single_concert_listing.append(date)
+                ticket_price = concert.find('h3', attrs={'class': 'price-range'}).text.strip()
+                single_concert_listing.append(ticket_price)
+
+                all_concert_listings.append(single_concert_listing)
+            except Exception as e:
+                logging.info('exception raised, but passed so program will run: %s' % e )
+                pass
+        return all_concert_listings
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import csv
 import logging
 from bs4 import BeautifulSoup
 
+from hidive import HiDiveShows
+
 
 def setup_logging():
     logging.basicConfig(
@@ -16,36 +18,10 @@ def main():
 
     logging.info('program starting')
 
-    # test one url to scrape concert listings
-    venue_page = 'http://www.hi-dive.com/'
-
-    # query the website and return the results to a new variable
-    page = urllib2.urlopen(venue_page).read()
-
-    # use BeautulSoup to parse the html and store its contents into a new variable
-    soup = BeautifulSoup(page, 'html.parser')
-
-    # this will create a BS concert section object that is iterable
-    concerts = soup.find_all('div', attrs={'class': "list-view-item"})
-    # print concert_details[0].prettify() 
-
-    all_concert_listings = [] # create a master list of all the concert event listings from sing_concert_listing
-    for concert in concerts:
-        try:
-            single_concert_listing = []  # create a concert list for each event
-            headliner = concert.find('h1', attrs={'class': 'headliners summary'}).text.strip()
-            single_concert_listing.append(headliner)
-            opening_act = concert.find('h2', attrs={'class': 'supports description'}).text.strip()
-            single_concert_listing.append(opening_act)
-            date = concert.find('h2', attrs={'class': 'dates'}).text.strip()
-            single_concert_listing.append(date)
-            ticket_price = concert.find('h3', attrs={'class': 'price-range'}).text.strip()
-            single_concert_listing.append(ticket_price)
-
-            all_concert_listings.append(single_concert_listing)
-        except Exception as e:
-            logging.info('exception raised, but passed so program will run: %s' % e )
-            pass
+    # instance of hiDiveShows class and pass in name and website url
+    hi_dive = HiDiveShows('hi-dive', 'http://www.hi-dive.com/')
+    hi_dive.getBSObject()
+    all_concert_listings = hi_dive.getConcertListings()
 
     # open a csv file and write out concert contents
     with open('hi-dive-listings.csv', 'wb') as csv_file:
@@ -58,4 +34,4 @@ def main():
 if __name__ == '__main__':
 
     setup_logging()
-    main()  
+    main()
